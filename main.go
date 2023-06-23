751e6bf7071a1f00c35ec7966aba4c09210b36e3
Youngjae Shin (신영재) <sdhkygg@gmail.com>
2023-06-23 20:10:15 +0900

    run using const and var

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
-import (
-  "fmt"
-  "go_nomad/something"
-)
+import "fmt"
 
 func main() {
-  fmt.Println("Hello, World!")
-  something.SayHello()
+  const name string = "nico"
+  var name2 string = "lynn"
+  name2 = "nico"
+  fmt.Println(name, name2)
+  // 축약형 but, 새로운 변수를 만들 때만 사용 가능, 함수 안에서만 사용 가능
+  name3 := "lynn"
+  fmt.Println(name3)
 }
