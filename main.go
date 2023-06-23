777a790a26f6d27a457ab9d6c3ee160cba344ec9
Youngjae Shin (신영재) <sdhkygg@gmail.com>
2023-06-23 20:32:18 +0900

    learn func input and output type

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+)
+
+
+func multiply(a, b int) int {
+  return a * b
+}
+
 
 func main() {
-  const name string = "nico"
-  var name2 string = "lynn"
-  name2 = "nico"
-  fmt.Println(name, name2)
-  // 축약형 but, 새로운 변수를 만들 때만 사용 가능, 함수 안에서만 사용 가능
-  name3 := "lynn"
-  fmt.Println(name3)
+  fmt.Println(multiply(2, 3))
 }
