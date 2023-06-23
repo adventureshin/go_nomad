a6b33652bc635a4a4e4174a59a673f2633eb057d
Youngjae Shin (신영재) <sdhkygg@gmail.com>
2023-06-23 21:00:42 +0900

    learn mulit input - main.go

--- main.go
+++ main.go
@@ -2,15 +2,13 @@ package main
 
 import (
   "fmt"
-  "strings"
 )
 
 
-func lenAndUpper(name string) (int, string) {
-  return len(name), strings.ToUpper(name)
+func repeatMe(words ...string) {
+  fmt.Println(words)
 }
 
 func main() {
-  totalLength, upperName := lenAndUpper("nico")
-  fmt.Println(totalLength, upperName)
+  repeatMe("nico", "lynn", "dal", "marl", "flynn")
 }
