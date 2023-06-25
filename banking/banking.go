fc56068a802f9d050c9d028d7d23296eb7447191
Youngjae Shin (신영재) <sdhkygg@gmail.com>
2023-06-25 21:18:40 +0900

    make banking package

---
+++ banking/banking.go
@@ -0,0 +1,13 @@
+package banking
+
+// Account struct
+type Account struct {
+  owner string
+  balance int
+}
+
+// NewAccount creates Account
+func NewAccount(owner string) *Account {
+  account := Account{owner: owner, balance: 0}
+  return &account
+}
