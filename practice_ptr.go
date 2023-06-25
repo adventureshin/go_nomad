cd93e6ebb5e8de1ca152e7c43b0a590cc8e25195
Youngjae Shin (신영재) <sdhkygg@gmail.com>
2023-06-25 21:18:16 +0900

    make practice_ptr.go

---
+++ practice_ptr.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func zeroval(ival int) {
+    ival = 0
+}
+
+func zeroptr(iptr *int) {
+    *iptr = 0
+}
+
+func main() {
+    i := 100
+    fmt.Println("초기 값 : ", i)
+
+    zeroval(i)
+    fmt.Println("zeroval : ", i)
+
+    zeroptr(&i)
+    fmt.Println("zeroptr : ", i)
+   fmt.Println("포인터 :", &i)
+}
