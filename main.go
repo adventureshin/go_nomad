d151a90f812e47403c88882e9959cc0842867c5e
Youngjae Shin (신영재) <sdhkygg@gmail.com>
2023-07-03 02:27:45 +0900

    using c to check urls

--- main.go
+++ main.go
@@ -2,32 +2,49 @@ package main
 
 import (
   "fmt"
-  "time"
+  "net/http"
+  "errors"
 )
 
-type people struct {
-  person string
-  age int
-}
-
+var errRequestFailed = errors.New("Request failed")
 
 func main() {
-  c := make(chan string)
-  peopleList := [2]people {}
-  peopleList[0] = people {"nico", 5}
-  peopleList[1] = people {"flynn", 1}
-  for _, person := range peopleList {
-    fmt.Println("start: " + person.person)
-    go isSexy(person.person, person.age, c)
+  urls := []string{
+    "https://www.airbnb.com/",
+    "https://www.google.com/",
+    "https://www.amazon.com/",
+    "https://www.reddit.com/",
+    "https://www.google.com/",
+    "https://soundcloud.com/",
+    "https://www.facebook.com/",
+    "https://www.instagram.com/",
+    "https://academy.nomadcoders.co/",
   }
-  fmt.Println(<-c)
-  fmt.Println(<-c)
+  cErr := make(chan error)
+  var result = make(map[string]string)
+  for _, url := range urls {
+    go hitUrl(url, cErr)
+  }
+  for _, url := range urls {
+    err := <- cErr
+    if err != nil {
+      result[url] = "FAILED"
+    } else {
+      result[url] = "OK"
+    }
+  }
+  for url, status := range result {
+    fmt.Println(url + ": " + status)
+  }
 }
 
-func isSexy(person string, delay int, c chan string){
-  time.Sleep(time.Second * time.Duration(delay))
-  fmt.Println("send to channel:" + person)
-  c <- person + " is Sexy"
+func hitUrl(url string, cErr chan error) {
+  fmt.Println("Checking:", url)
+  resp, err := http.Get(url)
+  if err != nil {
+    cErr <- errRequestFailed
+  } else if resp.StatusCode >= 400 {
+    cErr <- errRequestFailed
+  }
+  cErr <- nil
 }
-
-
